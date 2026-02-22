5ca4dc5ceb43527613f599daba019043d5fda03b
Prabhat Jha <prabhatjha1511@gmail.com>
2026-02-23 00:31:49 +0530

    Initial project structure setup

---
+++ backend/identity-service/src/test/java/com/sprintforge/identity/IdentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprintforge.identity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
